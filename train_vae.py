bf5d439fe271a81fb4045194b75a7f17f6d55888
Shreshth Tuli <shreshth.tuli@happening.xyz>
2023-07-10 13:57:18 +0100

    Update train_vae.py

--- train_vae.py
+++ train_vae.py
@@ -2,11 +2,11 @@ from utils import *
 from models import *
 import torch
 import pytorch_lightning as pl
-
+torch.cuda.empty_cache()
 
 vae = VAE(max_len=dm.dataset.max_len, vocab_len=len(dm.dataset.symbol_to_idx), 
           latent_dim=1024, embedding_dim=64)
-trainer = pl.Trainer(gpus=0, max_epochs=18, logger=pl.loggers.CSVLogger('logs'), enable_checkpointing=False)
+trainer = pl.Trainer(devices=[1], max_epochs=18, logger=pl.loggers.CSVLogger('logs'), enable_checkpointing=False)
 print('Training..')
 trainer.fit(vae, dm)
 print('Saving..')
